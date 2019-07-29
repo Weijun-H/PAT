682ff1eebe752a53266d75bd9c95bc6094f8fce3
hwj <huangweijun1001@gmail.com>
2019-07-29 09:25:14 +0800

    1012 start

---
+++ AdvancedLevel/1012.The Best Rank/cmake-build-debug/CMakeFiles/1012_The_Best_Rank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1012_The_Best_Rank.dir/main.cpp.o"
+  "1012_The_Best_Rank.pdb"
+  "1012_The_Best_Rank"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1012_The_Best_Rank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/1012.The Best Rank/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+struct Student{
+    char id[8];
+    int C,M,E,A;
+}stu[2010];
+int main() {
+    int N,M;
+    scanf("%N%M");
+    for (int i = 0; i <N ; ++i) {
+        
+    }
+}
