772a0cd67c5a3e034204a8946281f1b941aab6ce
hwj <huangweijun1001@gmail.com>
2019-07-30 13:10:19 +0800

    check 1016

--- AdvancedLevel/1012.The Best Rank/main.cpp
+++ AdvancedLevel/1012.The Best Rank/main.cpp
@@ -1,16 +1,52 @@
-#include <cstdio>
-#include <cstring>
+#include <cmath>
+#include <iostream>
 #include <algorithm>
 using namespace std;
 
 struct Student{
-    char id[8];
-    int C,M,E,A;
+    int id;
+    int grade[4];
 }stu[2010];
+
+char course[4] = {'A','C','M','E'};
+int Rank[10000000][4] = {0};
+int now;
+
+bool cmp(Student a,Student b){
+    return (a.grade[now]>b.grade[now]);
+}
 int main() {
-    int N,M;
-    scanf("%N%M");
-    for (int i = 0; i <N ; ++i) {
-        
+    int n,m;
+    scanf("%d %d",&n,&m);
+    for (int i = 0; i <n ; ++i) {
+        scanf("%d%d%d%d",&stu[i].id,&stu[i].grade[1],&stu[i].grade[2],&stu[i].grade[3]);
+        stu[i].grade[0] = round((stu[i].grade[1]+stu[i].grade[2]+stu[i].grade[3])/3.0)+0.5;
     }
+    for (int now = 0; now < 4; ++now) {
+        sort(stu,stu+n,cmp);
+        for (int j = 0; j < n; ++j) {
+            printf("%d %d %c\n",stu[j].id,stu[j].grade[now],course[now]);
+        }
+        printf("\n");
+
+        Rank[stu[0].id][now] =1;
+        for (int i = 1; i < n; i++) {
+            if (stu[i].grade[now]==stu[i-1].grade[now]) Rank[stu[i].id][now] = Rank[stu[i-1].id][now];
+            else Rank[stu[i].id][now] =i+1;
+        }
+    }
+    int query;
+    for (int i = 0; i <m ; ++i) {
+        scanf("%d",&query);
+        if(Rank[query][0]==0) printf("N/A\n");
+        else{
+            int k =0;
+            for (int j = 0; j < 4; ++j) {
+                if(Rank[query][j] < Rank[query][k]) k=j;
+            }
+            printf("%d %c %d\n",Rank[query][k],course[k],Rank[query][0]);
+        }
+
+    }
+    return 0;
 }
