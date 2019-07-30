01fdff5c16119082c1b72e44e620446be8fa7ba6
hwj <huangweijun1001@gmail.com>
2019-07-30 13:43:33 +0800

    check 1028

---
+++ AdvancedLevel/1028.List Sorting/cmake-build-debug/CMakeFiles/1028_List_Sorting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1028_List_Sorting.dir/main.cpp.o"
+  "1028_List_Sorting.pdb"
+  "1028_List_Sorting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1028_List_Sorting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
