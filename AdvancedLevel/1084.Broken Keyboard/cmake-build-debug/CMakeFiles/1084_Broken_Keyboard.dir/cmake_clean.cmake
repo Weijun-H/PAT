5f5ed9813c31254cb2042bb65895563e6c8adfc1
hwj <huangweijun1001@gmail.com>
2019-07-31 11:39:02 +0800

    check 1084

---
+++ AdvancedLevel/1084.Broken Keyboard/cmake-build-debug/CMakeFiles/1084_Broken_Keyboard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1084_Broken_Keyboard.dir/main.cpp.o"
+  "1084_Broken_Keyboard.pdb"
+  "1084_Broken_Keyboard"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1084_Broken_Keyboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
