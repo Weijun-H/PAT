a30f244c65b20b52960214c5afd1330c9320bd78
hwj <huangweijun1001@gmail.com>
2019-08-02 11:08:29 +0800

    check 1037

---
+++ AdvancedLevel/1037.Magic Coupon/cmake-build-debug/CMakeFiles/1037_Magic_Coupon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1037_Magic_Coupon.dir/main.cpp.o"
+  "1037_Magic_Coupon.pdb"
+  "1037_Magic_Coupon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1037_Magic_Coupon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
