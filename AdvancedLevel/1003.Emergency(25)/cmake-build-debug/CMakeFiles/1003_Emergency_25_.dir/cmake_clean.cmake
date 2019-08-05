f996e7b34603a1d9c80b726b1272ad1653b8e2dc
hwj <huangweijun1001@gmail.com>
2019-08-05 09:27:29 +0800

    check 1003

---
+++ AdvancedLevel/1003.Emergency(25)/cmake-build-debug/CMakeFiles/1003_Emergency_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1003_Emergency_25_.dir/main.cpp.o"
+  "1003_Emergency_25_.pdb"
+  "1003_Emergency_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1003_Emergency_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
