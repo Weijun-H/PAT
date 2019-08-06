e782b271662397857e49b4e229a9c0261eed7077
hwj <huangweijun1001@gmail.com>
2019-08-06 10:05:14 +0800

    check 1047

---
+++ AdvancedLevel/1047.Student for Course/cmake-build-debug/CMakeFiles/1047_Student_for_Course.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1047_Student_for_Course.dir/main.cpp.o"
+  "1047_Student_for_Course.pdb"
+  "1047_Student_for_Course"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1047_Student_for_Course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
