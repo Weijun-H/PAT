0b1b8bdbb2369439ac20f6bc325ae3c8d4ca85d6
hwj <huangweijun1001@gmail.com>
2019-08-06 09:46:36 +0800

    check 1039

---
+++ AdvancedLevel/1039.Course List for Student(25)/cmake-build-debug/CMakeFiles/1039_Course_List_for_Student_25_.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/huangweijun/Desktop/PAT/PAT/AdvancedLevel/1039.Course List for Student(25)/main.cpp" "/Users/huangweijun/Desktop/PAT/PAT/AdvancedLevel/1039.Course List for Student(25)/cmake-build-debug/CMakeFiles/1039_Course_List_for_Student_25_.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AdvancedLevel/1039.Course List for Student(25)/cmake-build-debug/CMakeFiles/1039_Course_List_for_Student_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1039_Course_List_for_Student_25_.dir/main.cpp.o"
+  "1039_Course_List_for_Student_25_.pdb"
+  "1039_Course_List_for_Student_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1039_Course_List_for_Student_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
