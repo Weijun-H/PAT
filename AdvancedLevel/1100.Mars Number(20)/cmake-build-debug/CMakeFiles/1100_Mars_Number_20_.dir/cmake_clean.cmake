94197d449b253513b9ea08582a0edf88ecde1b53
hwj <huangweijun1001@gmail.com>
2019-08-06 12:09:14 +0800

    check 1100

---
+++ AdvancedLevel/1100.Mars Number(20)/cmake-build-debug/CMakeFiles/1100_Mars_Number_20_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1100_Mars_Number_20_.dir/main.cpp.o"
+  "1100_Mars_Number_20_.pdb"
+  "1100_Mars_Number_20_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1100_Mars_Number_20_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
