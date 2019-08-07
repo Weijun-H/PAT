3b5a9f3847b44dc532f613a66f666db039e85200
hwj <huangweijun1001@gmail.com>
2019-08-07 11:54:08 +0800

    check 1022

---
+++ AdvancedLevel/1022.Digital Library(30)/cmake-build-debug/CMakeFiles/1022_Digital_Library_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1022_Digital_Library_30_.dir/main.cpp.o"
+  "1022_Digital_Library_30_.pdb"
+  "1022_Digital_Library_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1022_Digital_Library_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
