be88be92cd4b40dde39c57ff374a039c95193adf
hwj <huangweijun1001@gmail.com>
2019-08-12 11:49:01 +0800

    check 1074

---
+++ AdvancedLevel/1074.Reversing Linked List(25)/cmake-build-debug/CMakeFiles/1074_Reversing_Linked_List_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1074_Reversing_Linked_List_25_.dir/main.cpp.o"
+  "1074_Reversing_Linked_List_25_.pdb"
+  "1074_Reversing_Linked_List_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1074_Reversing_Linked_List_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
