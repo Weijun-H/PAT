faa2dfcb782747b3aa5b5473b26fa99657dc938b
hwj <huangweijun1001@gmail.com>
2019-08-13 10:30:32 +0800

    check 1052

---
+++ AdvancedLevel/1052.Linked Sorting(25)/cmake-build-debug/CMakeFiles/1052_Linked_Sorting_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1052_Linked_Sorting_25_.dir/main.cpp.o"
+  "1052_Linked_Sorting_25_.pdb"
+  "1052_Linked_Sorting_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1052_Linked_Sorting_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
