86fe07f819b5d3ccc7f8ab11468a20ee30101f1d
hwj <huangweijun1001@gmail.com>
2019-08-14 19:15:03 +0800

    check 1086

---
+++ AdvancedLevel/1086.Tree Traversals Again(25)/cmake-build-debug/CMakeFiles/1086_Tree_Traversals_Again_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1086_Tree_Traversals_Again_25_.dir/main.cpp.o"
+  "1086_Tree_Traversals_Again_25_.pdb"
+  "1086_Tree_Traversals_Again_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1086_Tree_Traversals_Again_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
