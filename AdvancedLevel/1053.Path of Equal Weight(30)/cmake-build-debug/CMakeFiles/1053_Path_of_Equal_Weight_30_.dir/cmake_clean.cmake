5d3db4be65b3781e0109ceb032a61526b59d766c
hwj <huangweijun1001@gmail.com>
2019-08-16 11:26:46 +0800

    check 1053

---
+++ AdvancedLevel/1053.Path of Equal Weight(30)/cmake-build-debug/CMakeFiles/1053_Path_of_Equal_Weight_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1053_Path_of_Equal_Weight_30_.dir/main.cpp.o"
+  "1053_Path_of_Equal_Weight_30_.pdb"
+  "1053_Path_of_Equal_Weight_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1053_Path_of_Equal_Weight_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/1053.Path of Equal Weight(30)/cmake-build-debug/CMakeFiles/1053_Path_of_Equal_Weight_30_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1053_Path_of_Equal_Weight_30_.
+# This may be replaced when dependencies are built.
