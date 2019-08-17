d72a8e6341fb277c4a42fd37636eac9c7e1b43a9
hwj <huangweijun1001@gmail.com>
2019-08-17 12:19:38 +0800

    check 1066

---
+++ AdvancedLevel/1066.Root of AVL Tree(25)/cmake-build-debug/CMakeFiles/1066_Root_of_AVL_Tree_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1066_Root_of_AVL_Tree_25_.dir/main.cpp.o"
+  "1066_Root_of_AVL_Tree_25_.pdb"
+  "1066_Root_of_AVL_Tree_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1066_Root_of_AVL_Tree_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
