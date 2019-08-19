93b9f0578c209393616381a141d33b077cb859d4
hwj <huangweijun1001@gmail.com>
2019-08-19 12:18:38 +0800

    check 1034

---
+++ AdvancedLevel/1034.Head of a Gang(30)/cmake-build-debug/CMakeFiles/1034_Head_of_a_Gang_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1034_Head_of_a_Gang_30_.dir/main.cpp.o"
+  "1034_Head_of_a_Gang_30_.pdb"
+  "1034_Head_of_a_Gang_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1034_Head_of_a_Gang_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
