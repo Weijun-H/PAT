8f9e93dfddc2e102a128f85f066cc0ecf650ee06
hwj <huangweijun1001@gmail.com>
2019-08-19 09:52:38 +0800

    check 1098

---
+++ AdvancedLevel/1098.Insertion or Heap Sort(25)/cmake-build-debug/CMakeFiles/1098_Insertion_or_Heap_Sort_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1098_Insertion_or_Heap_Sort_25_.dir/main.cpp.o"
+  "1098_Insertion_or_Heap_Sort_25_.pdb"
+  "1098_Insertion_or_Heap_Sort_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1098_Insertion_or_Heap_Sort_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
