fd30954b175a90b3b0acbe1f0c7936060d4eda83
hwj <huangweijun1001@gmail.com>
2019-08-19 10:41:41 +0800

    check 1013

---
+++ AdvancedLevel/1013.Battle over cities(25)/cmake-build-debug/CMakeFiles/1013_Battle_over_cities_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1013_Battle_over_cities_25_.dir/main.cpp.o"
+  "1013_Battle_over_cities_25_.pdb"
+  "1013_Battle_over_cities_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1013_Battle_over_cities_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/1013.Battle over cities(25)/cmake-build-debug/CMakeFiles/1013_Battle_over_cities_25_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1013_Battle_over_cities_25_.
+# This may be replaced when dependencies are built.
