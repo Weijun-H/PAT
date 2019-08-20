0c67ac474867d3f1f3236331571c017c619afeb5
hwj <huangweijun1001@gmail.com>
2019-08-20 10:00:36 +0800

    check 1034

---
+++ AdvancedLevel/1076.Forwards on Weibo(30)/cmake-build-debug/CMakeFiles/1076_Forwards_on_Weibo_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1076_Forwards_on_Weibo_30_.dir/main.cpp.o"
+  "1076_Forwards_on_Weibo_30_.pdb"
+  "1076_Forwards_on_Weibo_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1076_Forwards_on_Weibo_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/1076.Forwards on Weibo(30)/cmake-build-debug/CMakeFiles/1076_Forwards_on_Weibo_30_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1076_Forwards_on_Weibo_30_.
+# This may be replaced when dependencies are built.
