81680d49bf854a471efccdda16f73f68250ca04c
hwj <huangweijun1001@gmail.com>
2019-08-21 10:12:15 +0800

    check 1030

---
+++ AdvancedLevel/1030.Travel Plan(30)/cmake-build-debug/CMakeFiles/1030_Travel_Plan_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1030_Travel_Plan_30_.dir/main.cpp.o"
+  "1030_Travel_Plan_30_.pdb"
+  "1030_Travel_Plan_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1030_Travel_Plan_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
