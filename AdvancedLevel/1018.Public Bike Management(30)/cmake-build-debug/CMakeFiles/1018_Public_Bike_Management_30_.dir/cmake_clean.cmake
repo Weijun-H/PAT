5569499c3284e6d56e38de701a8bca384d66aa72
hwj <huangweijun1001@gmail.com>
2019-08-21 10:53:35 +0800

    check 1018

---
+++ AdvancedLevel/1018.Public Bike Management(30)/cmake-build-debug/CMakeFiles/1018_Public_Bike_Management_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1018_Public_Bike_Management_30_.dir/main.cpp.o"
+  "1018_Public_Bike_Management_30_.pdb"
+  "1018_Public_Bike_Management_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1018_Public_Bike_Management_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
