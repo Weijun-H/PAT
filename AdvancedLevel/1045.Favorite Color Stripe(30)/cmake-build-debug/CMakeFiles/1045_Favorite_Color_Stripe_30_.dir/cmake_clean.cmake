3057fcdfbcbf99b11acf2dfce735e1ea4cdb081a
hwj <huangweijun1001@gmail.com>
2019-08-22 10:56:02 +0800

    check 1045

---
+++ AdvancedLevel/1045.Favorite Color Stripe(30)/cmake-build-debug/CMakeFiles/1045_Favorite_Color_Stripe_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1045_Favorite_Color_Stripe_30_.dir/main.cpp.o"
+  "1045_Favorite_Color_Stripe_30_.pdb"
+  "1045_Favorite_Color_Stripe_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1045_Favorite_Color_Stripe_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
