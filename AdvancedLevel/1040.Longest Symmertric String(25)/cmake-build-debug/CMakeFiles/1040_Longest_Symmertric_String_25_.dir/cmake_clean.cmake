6d4a8d0b6fd61816f961cfb419edb2572ceb8d0b
hwj <huangweijun1001@gmail.com>
2019-08-22 12:56:09 +0800

    check 1040

---
+++ AdvancedLevel/1040.Longest Symmertric String(25)/cmake-build-debug/CMakeFiles/1040_Longest_Symmertric_String_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1040_Longest_Symmertric_String_25_.dir/main.cpp.o"
+  "1040_Longest_Symmertric_String_25_.pdb"
+  "1040_Longest_Symmertric_String_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1040_Longest_Symmertric_String_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
