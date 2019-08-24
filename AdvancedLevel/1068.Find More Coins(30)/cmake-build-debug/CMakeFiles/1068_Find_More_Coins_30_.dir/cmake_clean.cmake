3330679fa52522213703d7f7102121decbc00bad
hwj <huangweijun1001@gmail.com>
2019-08-24 09:48:46 +0800

    check 1068

---
+++ AdvancedLevel/1068.Find More Coins(30)/cmake-build-debug/CMakeFiles/1068_Find_More_Coins_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1068_Find_More_Coins_30_.dir/main.cpp.o"
+  "1068_Find_More_Coins_30_.pdb"
+  "1068_Find_More_Coins_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1068_Find_More_Coins_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
