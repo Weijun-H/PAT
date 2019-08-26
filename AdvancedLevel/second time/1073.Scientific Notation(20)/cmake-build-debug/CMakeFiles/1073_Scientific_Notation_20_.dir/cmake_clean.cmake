ed72088a81a33c01d2be608898e35575681ab247
hwj <huangweijun1001@gmail.com>
2019-08-26 11:24:55 +0800

    check 1073

---
+++ AdvancedLevel/second time/1073.Scientific Notation(20)/cmake-build-debug/CMakeFiles/1073_Scientific_Notation_20_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1073_Scientific_Notation_20_.dir/main.cpp.o"
+  "1073_Scientific_Notation_20_.pdb"
+  "1073_Scientific_Notation_20_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1073_Scientific_Notation_20_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
