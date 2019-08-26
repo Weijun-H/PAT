306ff7feb4fdf40fe7ae8d8453ed23ee1dabdcc3
hwj <huangweijun1001@gmail.com>
2019-08-26 19:02:22 +0800

    check 1082

---
+++ AdvancedLevel/second time/1082.Read Number in Chinese(25)/cmake-build-debug/CMakeFiles/1082_Read_Number_in_Chinese_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1082_Read_Number_in_Chinese_25_.dir/main.cpp.o"
+  "1082_Read_Number_in_Chinese_25_.pdb"
+  "1082_Read_Number_in_Chinese_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1082_Read_Number_in_Chinese_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
