80b6b1132989fcb051bf73ba11367dafc2fa4439
hwj <huangweijun1001@gmail.com>
2019-08-27 11:30:06 +0800

    check 1016

---
+++ AdvancedLevel/second time/1016.Phone Bill(25)/cmake-build-debug/CMakeFiles/1016_Phone_Bill_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1016_Phone_Bill_25_.dir/main.cpp.o"
+  "1016_Phone_Bill_25_.pdb"
+  "1016_Phone_Bill_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1016_Phone_Bill_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
