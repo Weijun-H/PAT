b86104e53c06f3caf608a34963b32241c15907b3
hwj <huangweijun1001@gmail.com>
2019-08-28 12:14:35 +0800

    check 1148

---
+++ AdvancedLevel/second time/1148.Werewolf(20)/cmake-build-debug/CMakeFiles/1148_Werewolf_20_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1148_Werewolf_20_.dir/main.cpp.o"
+  "1148_Werewolf_20_.pdb"
+  "1148_Werewolf_20_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1148_Werewolf_20_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
