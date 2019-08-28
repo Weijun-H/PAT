02cf183253032cfaa01585733dffaad868477b1f
hwj <huangweijun1001@gmail.com>
2019-08-28 12:44:59 +0800

    check 1144

---
+++ AdvancedLevel/second time/1144.The Missing Number(20)/cmake-build-debug/CMakeFiles/1144_The_Missing_Number_20_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1144_The_Missing_Number_20_.dir/main.cpp.o"
+  "1144_The_Missing_Number_20_.pdb"
+  "1144_The_Missing_Number_20_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1144_The_Missing_Number_20_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
