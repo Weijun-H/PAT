7c368a2d8c7223220d56ba563fe6fc2ef82e5801
hwj <huangweijun1001@gmail.com>
2019-08-29 10:16:30 +0800

    check 1084 twice

---
+++ AdvancedLevel/second time/1084.旧键盘(20)/cmake-build-debug/CMakeFiles/1084__20_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1084__20_.dir/main.cpp.o"
+  "1084__20_.pdb"
+  "1084__20_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1084__20_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
