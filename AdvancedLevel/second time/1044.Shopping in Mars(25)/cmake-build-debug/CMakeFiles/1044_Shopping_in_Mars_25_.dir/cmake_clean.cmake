382ba949c5ae58963685782b71a6b852dacc5407
hwj <huangweijun1001@gmail.com>
2019-08-30 10:25:26 +0800

    check 1044

---
+++ AdvancedLevel/second time/1044.Shopping in Mars(25)/cmake-build-debug/CMakeFiles/1044_Shopping_in_Mars_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1044_Shopping_in_Mars_25_.dir/main.cpp.o"
+  "1044_Shopping_in_Mars_25_.pdb"
+  "1044_Shopping_in_Mars_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1044_Shopping_in_Mars_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
