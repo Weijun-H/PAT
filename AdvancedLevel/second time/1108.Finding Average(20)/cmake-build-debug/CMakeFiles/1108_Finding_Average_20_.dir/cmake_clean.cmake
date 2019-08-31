e9ee2fa84f12e4e6c444b2b4dff2e8924108e85a
hwj <huangweijun1001@gmail.com>
2019-08-31 10:03:12 +0800

    check 1108

---
+++ AdvancedLevel/second time/1108.Finding Average(20)/cmake-build-debug/CMakeFiles/1108_Finding_Average_20_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1108_Finding_Average_20_.dir/main.cpp.o"
+  "1108_Finding_Average_20_.pdb"
+  "1108_Finding_Average_20_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1108_Finding_Average_20_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
