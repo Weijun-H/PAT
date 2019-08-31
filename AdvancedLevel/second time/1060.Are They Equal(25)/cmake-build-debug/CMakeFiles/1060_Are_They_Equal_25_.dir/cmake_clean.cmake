ddb7b543ed58fc08dae10846de65d91014f66a76
hwj <huangweijun1001@gmail.com>
2019-08-31 10:27:44 +0800

    check 1060

---
+++ AdvancedLevel/second time/1060.Are They Equal(25)/cmake-build-debug/CMakeFiles/1060_Are_They_Equal_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1060_Are_They_Equal_25_.dir/main.cpp.o"
+  "1060_Are_They_Equal_25_.pdb"
+  "1060_Are_They_Equal_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1060_Are_They_Equal_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/second time/1060.Are They Equal(25)/cmake-build-debug/CMakeFiles/1060_Are_They_Equal_25_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1060_Are_They_Equal_25_.
+# This may be replaced when dependencies are built.
