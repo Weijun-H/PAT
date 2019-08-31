151bd8a202957126c3b83cc5ce96f882977b0cf4
hwj <huangweijun1001@gmail.com>
2019-08-31 12:56:07 +0800

    check 1074

---
+++ AdvancedLevel/second time/1074.反转链表(25)/cmake-build-debug/CMakeFiles/1074__25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1074__25_.dir/main.cpp.o"
+  "1074__25_.pdb"
+  "1074__25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1074__25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
