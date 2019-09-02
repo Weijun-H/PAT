29a4b463e7972e2231edb364074ec23ade703735
hwj <huangweijun1001@gmail.com>
2019-09-02 12:32:34 +0800

    check 1043

---
+++ AdvancedLevel/second time/1043.Is it a Binary Search Tree(25)/cmake-build-debug/CMakeFiles/1043_Is_it_a_Binary_Search_Tree_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1043_Is_it_a_Binary_Search_Tree_25_.dir/main.cpp.o"
+  "1043_Is_it_a_Binary_Search_Tree_25_.pdb"
+  "1043_Is_it_a_Binary_Search_Tree_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1043_Is_it_a_Binary_Search_Tree_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/second time/1043.Is it a Binary Search Tree(25)/cmake-build-debug/CMakeFiles/1043_Is_it_a_Binary_Search_Tree_25_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1043_Is_it_a_Binary_Search_Tree_25_.
+# This may be replaced when dependencies are built.
