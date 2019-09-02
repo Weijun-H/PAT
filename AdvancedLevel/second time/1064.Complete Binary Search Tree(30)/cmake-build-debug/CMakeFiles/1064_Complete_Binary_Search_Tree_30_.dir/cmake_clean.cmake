dd5026de63183620829eab9b23cfdf7d722d7e85
hwj <huangweijun1001@gmail.com>
2019-09-02 13:29:27 +0800

    check 1064

---
+++ AdvancedLevel/second time/1064.Complete Binary Search Tree(30)/cmake-build-debug/CMakeFiles/1064_Complete_Binary_Search_Tree_30_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1064_Complete_Binary_Search_Tree_30_.dir/main.cpp.o"
+  "1064_Complete_Binary_Search_Tree_30_.pdb"
+  "1064_Complete_Binary_Search_Tree_30_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1064_Complete_Binary_Search_Tree_30_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
