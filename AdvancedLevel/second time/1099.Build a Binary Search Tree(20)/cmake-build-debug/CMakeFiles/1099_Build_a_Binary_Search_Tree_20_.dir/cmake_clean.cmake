fd416205f6d6aa70b0e2ae0d1c0769de67135852
hwj <huangweijun1001@gmail.com>
2019-09-03 10:00:25 +0800

    check 1099

---
+++ AdvancedLevel/second time/1099.Build a Binary Search Tree(20)/cmake-build-debug/CMakeFiles/1099_Build_a_Binary_Search_Tree_20_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1099_Build_a_Binary_Search_Tree_20_.dir/main.cpp.o"
+  "1099_Build_a_Binary_Search_Tree_20_.pdb"
+  "1099_Build_a_Binary_Search_Tree_20_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1099_Build_a_Binary_Search_Tree_20_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/second time/1099.Build a Binary Search Tree(20)/cmake-build-debug/CMakeFiles/1099_Build_a_Binary_Search_Tree_20_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1099_Build_a_Binary_Search_Tree_20_.
+# This may be replaced when dependencies are built.
