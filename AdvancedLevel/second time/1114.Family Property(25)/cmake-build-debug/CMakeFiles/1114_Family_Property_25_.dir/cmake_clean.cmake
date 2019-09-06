f4e4b921b6d58fc5f12656f835c729dd56628402
hwj <huangweijun1001@gmail.com>
2019-09-06 15:47:27 +0800

    check 1114

---
+++ AdvancedLevel/second time/1114.Family Property(25)/cmake-build-debug/CMakeFiles/1114_Family_Property_25_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1114_Family_Property_25_.dir/main.cpp.o"
+  "1114_Family_Property_25_.pdb"
+  "1114_Family_Property_25_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1114_Family_Property_25_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdvancedLevel/second time/1114.Family Property(25)/cmake-build-debug/CMakeFiles/1114_Family_Property_25_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1114_Family_Property_25_.
+# This may be replaced when dependencies are built.
